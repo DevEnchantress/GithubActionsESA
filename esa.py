e52f817cedf86f18570a5fe2156dedf9dd8c7398
DevEnchantress <iamst3ff@gmail.com>
2024-12-08 13:14:20 +0100

    Update esa.py

--- esa.py
+++ esa.py
@@ -1,1 +1,9 @@
+import datetime
+
+def get_wochentage():
+    wochentage = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]
+  return wochentage[datetime.datetime.now().wochentage()]
+
 print ("Hallo, schön, dass du da bist.")
+print (f"Heute  ist {get_wochentage()}.")
+print ("Hab noch einen schönen Tag.")
